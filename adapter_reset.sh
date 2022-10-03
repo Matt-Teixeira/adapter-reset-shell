5e3e6cdea5ba892ff16078f79215eab0948e1f0a
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-10-03 15:27:04 +0000

    Reset network adapters

---
+++ adapter_reset.sh
@@ -0,0 +1,15 @@
+output=$(ifconfig -a)
+adapters=($(echo $output | grep -Po 'en\w+'))
+
+for i in "${adapters[@]}"; do
+    adapter_blocks=$(echo $output | grep -Po "${i}.*?collisions")
+    is_connected=$(echo $adapter_blocks | grep -Po 'inet[^\d]')
+    if [ $is_connected ]; then
+        echo $i is connected
+    else
+        echo $i is not connected
+        sudo ifconfig $i down
+        sudo ifconfig $i up
+    # sudo ifconfig $i 192.168.0.2 netmask 255.255.255.0
+    fi
+done

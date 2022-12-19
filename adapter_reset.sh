db6106a1822d3e781ab545f742e36e0ecb981413
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-12-19 17:09:54 +0000

    Added logic to reassign static ip

--- adapter_reset.sh
+++ adapter_reset.sh
@@ -12,4 +12,12 @@ for i in "${adapters[@]}"; do
         sudo ifconfig $i up
     # sudo ifconfig $i 192.168.0.2 netmask 255.255.255.0
     fi
+    adapter_blocks=$(echo $output | grep -Po "${i}.*?collisions")
+    is_connected=$(echo $adapter_blocks | grep -Po 'inet[^\d]')
+    if [ $is_connected ]; then
+        echo $i is connected
+    else
+        echo $i is not connected
+        sudo ifconfig $i 192.168.0.2 netmask 255.255.255.0
+    fi
 done

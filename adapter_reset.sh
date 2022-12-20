e31e3fdbc5ca026fd1f4c7dc69ad89a1ca6bd90e
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2022-12-20 15:35:31 +0000

    Refactor for clear feedback

--- adapter_reset.sh
+++ adapter_reset.sh
@@ -9,7 +9,9 @@ for i in "${adapters[@]}"; do
     else
         echo $i is not connected
         sudo ifconfig $i down
+        echo $1 brought down
         sudo ifconfig $i up
+        echo $1 brought up
     # sudo ifconfig $i 192.168.0.2 netmask 255.255.255.0
     fi
 done
@@ -19,9 +21,9 @@ for i in "${adapters[@]}"; do
     adapter_blocks=$(echo $output | grep -Po "${i}.*?collisions")
     is_connected=$(echo $adapter_blocks | grep -Po 'inet[^\d]')
     if [ $is_connected ]; then
-        echo $i is connected
+        echo $i no need to reset static IP
     else
-        echo $i is not connected
+        echo $i reseting static IP to 192.168.0.2 netmask 255.255.255.0 
         sudo ifconfig $i 192.168.0.2 netmask 255.255.255.0
     fi
 done
